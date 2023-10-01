0fc8447a4650dea881deeee2e78fefee9ce5b7f0
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-10-01 17:03:04 +0300

    Play 'got fish' sample when catching fish

--- Assets/Scenes/Items/FishingShip.gd
+++ Assets/Scenes/Items/FishingShip.gd
@@ -1,12 +1,14 @@
 extends "res://Assets/Scenes/Item.gd"
 
 class_name FishingShip
+@onready var soundEngine = $/root/Main/SoundEngine
 
 func on_collide(other: Item):
 	if is_instance_of(other, Fish):
 		Global.collect_points(Global.POINTS__FISH_COLLECTION)
 		Global.collect_fish(2)
 		other.destroy()
+		soundEngine.play_got_fish()
 	if is_instance_of(other, PirateShip):
 		Global.collect_points(Global.POINTS__FISHING_SHIP_DESTROY)
 		other.destroy()
