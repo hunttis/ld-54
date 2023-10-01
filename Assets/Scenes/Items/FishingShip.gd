2bc22f3d54885f301e8838d09b6c93bb24d7dcb0
Joni Katajamäki <joni.katajamaki@icloud.com>
2023-10-01 17:03:41 +0300

    Inverted fish and 2 health for fishing boats

--- Assets/Scenes/Items/FishingShip.gd
+++ Assets/Scenes/Items/FishingShip.gd
@@ -2,6 +2,10 @@ extends "res://Assets/Scenes/Item.gd"
 
 class_name FishingShip
 
+var _health = 2
+
+var _hit_color = Color(0.9, 0.3, 0.0)
+
 func on_collide(other: Item):
 	if is_instance_of(other, Fish):
 		Global.collect_points(Global.POINTS__FISH_COLLECTION)
@@ -9,8 +13,11 @@ func on_collide(other: Item):
 		other.destroy()
 	if is_instance_of(other, PirateShip):
 		Global.collect_points(Global.POINTS__FISHING_SHIP_DESTROY)
+		_health -= 1
+		self.modulate = _hit_color
 		other.destroy()
-		self.destroy()
+		if _health <= 0:
+			self.destroy()
 	if is_instance_of(other, Whirlpool):
 		Global.collect_points(Global.POINTS__FISHING_SHIP_COLLISION_WITH_A_WHIRLPOOL)
 		self.destroy()
