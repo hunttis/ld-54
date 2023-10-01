57b8eecfb53553edf1331a5b21cf621ee193ebf7
Ville Nuutinen <hunttis@me.com>
2023-10-01 16:34:37 +0300

    Tutorial and game over screen

--- Assets/Scenes/Tutorial.gd
+++ Assets/Scenes/Tutorial.gd
@@ -1,2 +1,42 @@
 extends Control
 
+@onready var pirateTutorial = $VBoxContainer/TopTutorial/PirateTutorial
+@onready var fishingShipTutorial = $VBoxContainer/HBoxContainer/FishingShipTutorial
+@onready var fishTutorial = $VBoxContainer/HBoxContainer/FishTutorial
+@onready var villageTutorial = $VBoxContainer/BottomTutorial/VillageTutorial
+@onready var movementTutorial = $VBoxContainer/MiddleTutorial/MovementTutorial
+
+
+var timer = 0
+var howLongToShowOneTutorial = 5
+
+func _ready():
+	hideAllTutorials()
+
+func _process(delta):
+	timer += delta
+	if Global.show_tutorial:
+		if timer < howLongToShowOneTutorial:
+			hideAllTutorials()
+			pirateTutorial.visible = true
+		elif timer < howLongToShowOneTutorial * 2:
+			hideAllTutorials()
+			fishingShipTutorial.visible = true
+		elif timer < howLongToShowOneTutorial * 3:
+			hideAllTutorials()
+			fishTutorial.visible = true
+		elif timer < howLongToShowOneTutorial * 4:
+			hideAllTutorials()
+			villageTutorial.visible = true
+		elif timer < howLongToShowOneTutorial * 5:
+			hideAllTutorials()
+			movementTutorial.visible = true
+		elif timer < howLongToShowOneTutorial * 6:
+			hideAllTutorials()
+
+func hideAllTutorials():
+		pirateTutorial.visible = false
+		fishingShipTutorial.visible = false
+		fishTutorial.visible = false
+		villageTutorial.visible = false
+		movementTutorial.visible = false
