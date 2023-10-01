aa58a6b1882c263221d99b74f9740ad0705ad61d
Mikko <mkarvonen34@gmail.com>
2023-10-01 15:48:27 +0300

    Add tentacles

--- Assets/Scenes/Items/Rock.gd
+++ Assets/Scenes/Items/Rock.gd
@@ -1,10 +1,6 @@
-extends "res://Assets/Scenes/Item.gd"
+extends "res://Assets/Scenes/Floating.gd"
 
 class_name Rock
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	self.modulate = Color.DARK_GRAY
 
 func can_move(other: Item, items: GridItems) -> bool:
 	return false
