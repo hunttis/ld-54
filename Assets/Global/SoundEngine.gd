c80fc375fd7d3007dc5599c66aa4a36d18e6facc
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-10-01 19:16:13 +0300

    Play sound when sailing to a whirlpool

--- Assets/Global/SoundEngine.gd
+++ Assets/Global/SoundEngine.gd
@@ -6,6 +6,7 @@ var rng = RandomNumberGenerator.new()
 @onready var splash_players = [$Splash01, $Splash02, $Splash03, $Splash04]
 @onready var got_fish_players = [$GotFish01, $GotFish02, $GotFish03, $GotFish04, $GotFish05, $GotFish06, $GotFish07, $GotFish08]
 @onready var arrrrr_players = [$Arrrrr01, $Arrrrr02]
+@onready var oh_no_players = [$OhNo01, $OhNo02, $OhNo03, $OhNo04, $OhNo05, $OhNo06]
 @onready var thats_it_players = [$ThatsIt01, $ThatsIt02, $ThatsIt03, $ThatsIt04]
 
 func _ready():
@@ -31,6 +32,10 @@ func play_got_fish():
 func play_arrrrr():
 	var random_index = rng.randi_range(0, arrrrr_players.size() - 1)
 	arrrrr_players[random_index].play()
+
+func play_oh_no():
+	var random_index = rng.randi_range(0, oh_no_players.size() - 1)
+	oh_no_players[random_index].play()
 
 func play_thats_it():
 	var random_index = rng.randi_range(0, thats_it_players.size() - 1)
--- Assets/Scenes/Items/FishingShip.gd
+++ Assets/Scenes/Items/FishingShip.gd
@@ -23,6 +23,7 @@ func on_collide(other: Item):
 	if is_instance_of(other, Whirlpool):
 		Global.collect_points(Global.POINTS__FISHING_SHIP_COLLISION_WITH_A_WHIRLPOOL)
 		self.destroy()
+		soundEngine.play_oh_no()
 
 func can_move(other: Item, items: GridItems) -> bool:
 	return true
