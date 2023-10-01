a7897b88eca6eaa28dbcfb5a27244ea488da1b1f
Mika Viljanen <mika.viljanen@wunderdog.fi>
2023-10-01 16:40:37 +0300

    Play fish-catching sample (on every move...)

--- Assets/Global/SoundEngine.gd
+++ Assets/Global/SoundEngine.gd
@@ -1,12 +1,19 @@
 extends Node
 
 var master_audio_bus = AudioServer.get_bus_index("Master")
+var rng = RandomNumberGenerator.new()
 
-func play_music():
-	$Music.play()
+@onready var got_fish_players = [$GotFish01, $GotFish02, $GotFish03, $GotFish04, $GotFish05, $GotFish06, $GotFish07, $GotFish08]
 
 func is_muted():
 	return AudioServer.is_bus_mute(master_audio_bus)
 
 func toggle_mute():
 	AudioServer.set_bus_mute(master_audio_bus, not is_muted())
+
+func play_music():
+	$Music.play()
+
+func play_got_fish():
+	var random_index = rng.randi_range(0, got_fish_players.size() - 1)
+	got_fish_players[random_index].play()
